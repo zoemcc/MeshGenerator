9559d90de13d3e42b5227de2bf6b97c592301fc3
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-10-20 04:14:53 -0700

    added radial grid

--- MeshGenerator.hs
+++ MeshGenerator.hs
@@ -46,6 +46,28 @@ squareGrid axisResolution = Mesh verts tris
         tris = fromList $ P.concatMap takeOffSetGiveTris allPairs
         verts = fromList [Vertex vx 0.0 vz | vx <- axisRange, vz <- axisRange]
 
+radialGrid :: Int -> Mesh Double
+radialGrid axisResolution = Mesh verts tris
+    where 
+
+        axisRange :: [Double]
+        axisRange = map (\t -> t / (fromIntegral (axisResolution - 1))) (P.take axisResolution [1.0, 2.0 ..])
+
+        thetaRange :: [Double]
+        thetaRange = map (\t -> 2.0 * pi * t / (fromIntegral (axisResolution - 1))) (P.take axisResolution [0.0, 1.0 ..])
+
+        firstRowPairs = pairs $ P.take axisResolution [1, 2 ..]
+        colPairs      = pairs $ P.take axisResolution [0, axisResolution ..]
+        allPairs      = [(rowOffset, colOffset) | colOffset <- colPairs, rowOffset <- firstRowPairs]
+
+        takeOffSetGiveTris :: ((Int, Int), (Int, Int)) -> [Triangle]
+        --takeOffSetGiveTris ((r0, r1), (c0, c1)) | c0 == 0 = [(Triangle (r0 + c1) (r1 + c0) (r1 + c1))] -- get rid of multiple 0 radius vertices
+        --takeOffSetGiveTris ((r0, r1), (c0, c1)) | otherwise = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
+        takeOffSetGiveTris ((r0, r1), (c0, c1)) = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
+
+        tris = (fromList $ P.concatMap takeOffSetGiveTris allPairs) >< fromList [Triangle (S.length verts) r1 r0 | (r0, r1) <- firstRowPairs]
+        verts = (fromList [Vertex (radius * cos theta) 0.0 (radius * sin theta) | radius <- axisRange, theta <- thetaRange]) |> Vertex 0.0 0.0 0.0
+
 
         --tris = fronList [Triangle 
 
@@ -63,6 +85,8 @@ main = do
     meshToObj testSquare "testSquare.obj"
     print testSquare
     print (squareGrid 3)
-    meshToObj (squareGrid 225) "squareGrid225.obj"
-    print (pairs [1,2,3])
+    print (radialGrid 4)
+    --meshToObj (squareGrid 225) "squareGrid225.obj"
+    meshToObj (radialGrid 250) "radialGrid250.obj"
+    --print (pairs [1,2,3])
 
