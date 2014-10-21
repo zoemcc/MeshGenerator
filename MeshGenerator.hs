05637eef11cf52cd6cc8b18a1403dc27e1692b91
Zoe McCarthy <zoemccarthy12@gmail.com>
2014-10-20 23:47:55 -0700

    refactoring

--- MeshGenerator.hs
+++ MeshGenerator.hs
@@ -2,7 +2,6 @@ import Data.Sequence as S
 import Data.List
 import Data.Foldable
 import System.IO
---import qualified Data.Text as T
 import Prelude as P
 
 data Vertex a = Vertex a a a
@@ -36,46 +35,38 @@ squareGrid axisResolution = Mesh verts tris
         axisRange :: [Double]
         axisRange = map (\t -> 2.0 * (t - 0.5 * fromIntegral (axisResolution - 1)) / (fromIntegral (axisResolution - 1))) (P.take axisResolution [0.0, 1.0 ..])
 
-        firstRowPairs = pairs $ P.take axisResolution [1, 2 ..]
-        colPairs      = pairs $ P.take axisResolution [0, axisResolution ..]
-        allPairs      = [(rowOffset, colOffset) | colOffset <- colPairs, rowOffset <- firstRowPairs]
-
-        takeOffSetGiveTris :: ((Int, Int), (Int, Int)) -> [Triangle]
-        takeOffSetGiveTris ((r0, r1), (c0, c1)) = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
-
-        tris = fromList $ P.concatMap takeOffSetGiveTris allPairs
+        tris = fromList $ P.concatMap takeOffSetGiveTris $ allPairs axisResolution
         verts = fromList [Vertex vx 0.0 vz | vx <- axisRange, vz <- axisRange]
 
 radialGrid :: Int -> Mesh Double
 radialGrid axisResolution = Mesh verts tris
     where 
 
-        axisRange :: [Double]
-        axisRange = map (\t -> t / (fromIntegral (axisResolution - 1))) (P.take axisResolution [1.0, 2.0 ..])
+        radiusRange :: [Double]
+        radiusRange = map (\t -> t / (fromIntegral (axisResolution - 1))) (P.take axisResolution [1.0, 2.0 ..])
 
         thetaRange :: [Double]
         thetaRange = map (\t -> 2.0 * pi * t / (fromIntegral (axisResolution - 1))) (P.take axisResolution [0.0, 1.0 ..])
 
-        firstRowPairs = pairs $ P.take axisResolution [1, 2 ..]
-        colPairs      = pairs $ P.take axisResolution [0, axisResolution ..]
-        allPairs      = [(rowOffset, colOffset) | colOffset <- colPairs, rowOffset <- firstRowPairs]
+        tris = (fromList $ P.concatMap takeOffSetGiveTris $ allPairs axisResolution) >< --all but center
+                fromList [Triangle (S.length verts) r1 r0 | (r0, r1) <- firstRowPairs axisResolution] --center tris
+        verts = (fromList [Vertex (radius * cos theta) 0.0 (radius * sin theta) | radius <- radiusRange, theta <- thetaRange]) |> Vertex 0.0 0.0 0.0
 
-        takeOffSetGiveTris :: ((Int, Int), (Int, Int)) -> [Triangle]
-        --takeOffSetGiveTris ((r0, r1), (c0, c1)) | c0 == 0 = [(Triangle (r0 + c1) (r1 + c0) (r1 + c1))] -- get rid of multiple 0 radius vertices
-        --takeOffSetGiveTris ((r0, r1), (c0, c1)) | otherwise = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
-        takeOffSetGiveTris ((r0, r1), (c0, c1)) = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
+takeOffSetGiveTris :: ((Int, Int), (Int, Int)) -> [Triangle]
+takeOffSetGiveTris ((r0, r1), (c0, c1)) = [(Triangle (r0 + c0) (r1 + c0) (r0 + c1)), (Triangle (r0 + c1) (r1 + c0) (r1 + c1))]
 
-        tris = (fromList $ P.concatMap takeOffSetGiveTris allPairs) >< fromList [Triangle (S.length verts) r1 r0 | (r0, r1) <- firstRowPairs]
-        verts = (fromList [Vertex (radius * cos theta) 0.0 (radius * sin theta) | radius <- axisRange, theta <- thetaRange]) |> Vertex 0.0 0.0 0.0
+firstRowPairs :: Int -> [(Int, Int)]
+firstRowPairs resolution = pairs $ P.take resolution [1, 2 ..]
 
+colPairs :: Int -> [(Int, Int)]
+colPairs resolution      = pairs $ P.take resolution [0, resolution ..]
 
-        --tris = fronList [Triangle 
+allPairs :: Int -> [((Int, Int), (Int, Int))]
+allPairs resolution      = [(rowOffset, colOffset) | colOffset <- colPairs resolution, rowOffset <- firstRowPairs resolution]
 
 pairs :: [a] -> [(a, a)]
 pairs xs@(y:ys) = P.zip xs ys
 pairs _         = []
-
-        
 
 meshToObj :: (Show a) => Mesh a -> String -> IO ()
 meshToObj mesh filename = writeFile filename (show mesh ++ "\n")
@@ -84,8 +75,8 @@ main :: IO ()
 main = do 
     meshToObj testSquare "testSquare.obj"
     print testSquare
-    print (squareGrid 3)
-    print (radialGrid 4)
+    print $ squareGrid 3
+    print $ radialGrid 4
     --meshToObj (squareGrid 225) "squareGrid225.obj"
     meshToObj (radialGrid 250) "radialGrid250.obj"
     --print (pairs [1,2,3])
